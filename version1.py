2e07cfc01bbec522037ae9302eb4d49e750c0bf0
hajra202134 <147387321+hajra202134@users.noreply.github.com>
2023-11-20 17:01:07 +0500

    Create version1.py
    this is hajra's version.

---
+++ version1.py
@@ -0,0 +1,29 @@
+import math
+#Addition
+def add(num1,num2):
+    return num1+num2
+ans=add(5,3)
+print("result of addition is: ",ans)
+#Subtraction
+def sub(num1,num2):
+    return num1-num2
+ans1=sub(5,3)
+print("result of subtraction is: ",ans1)
+#Multiplication
+def mul(num1,num2):
+    return num1*num2
+ans2=mul(5,3)
+print("result of multiplication is is: ",ans2)
+#Division
+def division(num1,num2):
+    return num1/num2
+ans3=division(5,3)
+print("result of subtraction is: ",ans3)
+#squareRoot
+def sqroot(num1):
+    return math.sqrt(num1)
+ans4=sqroot(49)
+print("Square root of the given number is: ",ans4)
+ 
+
+
